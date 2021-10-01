072d62b6673b3149ed46373ad54e8b793b085547
myrzha <mirza@blendmedia.co.id>
2021-10-01 18:20:39 +0700

    Update

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var path        = require('path');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
-//app.use('/', express.static(path.join(__dirname, 'public')));
-//app.use('/assets', express.static(path.join(__dirname, './node_modules/@salesforce-ux/design-system/assets')));
-//app.get('/', "./customActivity.js")
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, './node_modules/@salesforce-ux/design-system/assets')));
+//app.use('/', "/public/customActivity.js")
 
 app.get('/', function(req, res){
-   res.send(express.static(path.join(__dirname, './index.html')));
+   res.send(express.static(path.join(__dirname, './public/index.html')));
 })
 
 http.createServer(app).listen(app.get('port'), function(){
